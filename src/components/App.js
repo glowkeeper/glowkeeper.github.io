8412ac7dfe8b8e0ab0910ad723858eb4fdec4dfa
steve <steve.huckle@gmail.com>
2022-01-12 16:12:10 +0000

    toggle theme

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import { Router } from './layout/Router'
 import { Footer } from './layout/Footer'
 
 export const App = () => {
+  
   return (
     <div id="content">
       <Router />
--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,7 +1,20 @@
+import { useState } from 'react';
+
 export const Footer = () => {
+  const [lightTheme, setLightTheme] = useState(true)
+
+  const root = document.documentElement;
+
   return (
     <footer>
-      <p>footer</p>
+      <button
+        onClick={() => {
+          const isLight = !lightTheme
+          const theme = isLight ? 'light' : 'dark'          
+          root.setAttribute('data-theme', theme);
+          setLightTheme(isLight)
+        }}
+      >Toggle Dark Mode</button>
     </footer>
   );
 }
