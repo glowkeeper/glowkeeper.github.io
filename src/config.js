b7cf81508a6359f24daf64c47a8f876fdfe0635e
glowkeeper <steve.huckle@gmail.com>
2023-03-31 11:47:44 +0100

    new blog entry

--- src/config.js
+++ src/config.js
@@ -369,6 +369,13 @@ export const blogSections = {
     subText: "How to Become an Expert on A.I",
     route: `${LocalRoutes.blog}/26March2023`,
     content: "/assets/posts/blog/26March2023.md"
+  },
+  "31032023": {
+    id: "blog-31032023",
+    title: "31st March 2023",
+    subText: "Elon Musk and Others Call for Pause on A.I., Citing ‘Profound Risks to Society’",
+    route: `${LocalRoutes.blog}/31March2023`,
+    content: "/assets/posts/blog/31March2023.md"
   }
 }
 
